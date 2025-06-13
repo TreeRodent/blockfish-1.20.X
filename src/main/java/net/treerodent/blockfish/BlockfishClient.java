89c779e494edb97c3362748b69a01b6bb318f279
Elesdina <146481657+Elesdina@users.noreply.github.com>
2025-06-13 15:05:16 -0500

    Initial commit

---
+++ src/main/java/net/treerodent/blockfish/BlockfishClient.java
@@ -0,0 +1,10 @@
+package net.treerodent.blockfish;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlockfishClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
